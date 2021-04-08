df405ee2dfcb8dd4b2aaff35b0d8997023b559b4
RiccardoTonioloDev <ssctonioloriccardo@gmail.com>
2021-04-08 20:19:30 +0200

    Creazione del nuovo comparto grafico del sito. Sperimentazioni sulla pagina di login.

---
+++ EDUChainV2/Pages/Login&Register.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Styles/Login&Register.css">
+    <title>Login & Register</title>
+</head>
+<body>
+    <div class="navbar">
+        <div class="sideLeft">
+            <img class="logo" src="../Images/EduchainLogo-BlackV.png" alt="Logo">
+            <div class="nav-text">EDUChain</div>
+        </div>
+        <ul class="sideRight">
+            <li>
+                <a class="nav-button" href="#">
+                    <div>Homepage</div>
+                    <div class="icon"></div>
+                </a>     
+            </li>
+            <li>
+                <a class="nav-button" href="#">
+                    <div>Accedi</div>
+                    <div class="icon"></div>
+                </a>    
+            </li>
+        </ul>
+    </div>
+    
+</body>
+</html>
