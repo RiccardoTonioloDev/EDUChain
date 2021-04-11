fcf74995923e5cb08b51614ad50c852b944b505a
RiccardoTonioloDev <ssctonioloriccardo@gmail.com>
2021-04-11 15:32:56 +0200

    Creata la pagina del saldo, con relativo css.

---
+++ EDUChainV2/Pages/PersonalDashboard/personalDashboardSaldo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../Styles/Login&Register.css">
+    <title>Login & Register</title>
+</head>
+<body>
+    <div class="navbar">
+        <div class="sideLeft">
+            <img class="logo" src="../../Images/EduchainLogo-BlackV.png" alt="Logo">
+            <div class="nav-text">EDUChain</div>
+        </div>
+        <ul class="sideRight">
+            <li>
+                <a class="nav-button" href="#">
+                    <img class="icon" src="../../Images/dog-house-2white.png" alt="Home">
+                    <div>Homepage</div>
+                </a>     
+            </li>
+            <li>
+                <a class="nav-button" href="#">
+                    <img class="icon" src="../../Images/login-3white.png" alt="Accedi">
+                    <div>Accedi</div>
+                </a>    
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
