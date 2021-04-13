e463779c0a95b5d07efaa0e54fecd84b325b4706
RiccardoTonioloDev <ssctonioloriccardo@gmail.com>
2021-04-13 17:08:54 +0200

    other little aestetical changes

--- EDUChainV2/Pages/PersonalDashboard/personalDashboardSaldo.php
+++ EDUChainV2/Pages/PersonalDashboard/personalDashboardSaldo.php
@@ -16,37 +16,37 @@
         <div class="sideRight">
             <ul class="nav-links">
             <li>
-                    <a class="nav-button" href="#">
+                    <a class="nav-button" href="personalDashboardSaldo.php">
                         <img class="icon" src="../../Images/EduchainLogo.png" alt="Saldo">
                         <div>Saldo</div>
                     </a>     
                 </li>
                 <li>
-                    <a class="nav-button" href="#">
+                    <a class="nav-button" href="personalDashboardInvia.php">
                         <img class="icon" src="../../Images/send-email-1-white.png" alt="Invia">
                         <div>Invia</div>
                     </a>    
                 </li>
                 <li>
-                    <a class="nav-button" href="#">
+                    <a class="nav-button" href="personalDashboardRicevi.php">
                         <img class="icon" src="../../Images/email-action-receive-white.png" alt="Ricevi">
                         <div>Ricevi</div>
                     </a>    
                 </li>
                 <li>
-                    <a class="nav-button" href="#">
+                    <a class="nav-button" href="personalDashboardGenera.php">
                         <img class="icon" src="../../Images/style-two-pin-add-white.png" alt="Genera">
                         <div>Genera</div>
                     </a>    
                 </li>
                 <li>
-                    <a class="nav-button" href="#">
+                    <a class="nav-button" href="personalDashboardRitira.php">
                         <img class="icon" src="../../Images/style-two-pin-minus-white.png" alt="Ritira">
                         <div>Ritira</div>
                     </a>    
                 </li>
                 <li>
-                    <a class="nav-button" href="#">
+                    <a class="nav-button" href="personalDashboardBlockchain.php">
                         <img class="icon" src="../../Images/app-window-four-white.png" alt="Blockchain">
                         <div>Blockchain</div>
                     </a>    
@@ -73,7 +73,7 @@
                     if(link.style.animation){
                         link.style.animation = "";
                     }else{
-                        link.style.animation = "navLinkFade 0.5s ease forwards "+(index / 7)+"s";
+                        link.style.animation = "navLinkFade 0.3s ease forwards "+(index / 15)+"s";
                     }
                 });
                 burger.classList.toggle("toggle");
