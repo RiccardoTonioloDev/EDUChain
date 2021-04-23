66b922db29028974da39a12ce550e4f46a65b5e8
RiccardoTonioloDev <ssctonioloriccardo@gmail.com>
2021-04-23 11:12:21 +0200

    Creazione della pagina Genera e della pagina Ritira, con retrocompatibilità al backend.

---
+++ EDUChainV2/Pages/PersonalDashboard/personalDashboardRitira.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../Styles/personalDashboardGenera.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="navbar">
+        <div class="sideLeft">
+            <img class="logo" src="../../Images/EduchainLogo-BlackV.png" alt="Logo">
+            <div class="nav-text">EDUChain</div>
+        </div>
+        <div class="sideRight">
+            <ul class="nav-links">
+            <li>
+                    <a class="nav-button" href="personalDashboardSaldo.php">
+                        <img class="icon" src="../../Images/EduchainLogo.png" alt="Saldo">
+                        <div>Saldo</div>
+                    </a>     
+                </li>
+                <li>
+                    <a class="nav-button" href="personalDashboardInvia.php">
+                        <img class="icon" src="../../Images/send-email-1-white.png" alt="Invia">
+                        <div>Invia</div>
+                    </a>    
+                </li>
+                <li>
+                    <a class="nav-button" href="personalDashboardRicevi.php">
+                        <img class="icon" src="../../Images/email-action-receive-white.png" alt="Ricevi">
+                        <div>Ricevi</div>
+                    </a>    
+                </li>
+                <li>
+                    <a class="nav-button" href="personalDashboardGenera.php">
+                        <img class="icon" src="../../Images/style-two-pin-add-white.png" alt="Genera">
+                        <div>Genera</div>
+                    </a>    
+                </li>
+                <li>
+                    <a class="nav-button" href="personalDashboardRitira.php">
+                        <img class="icon" src="../../Images/style-two-pin-minus-white.png" alt="Ritira">
+                        <div>Ritira</div>
+                    </a>    
+                </li>
+                <li>
+                    <a class="nav-button" href="personalDashboardBlockchain.php">
+                        <img class="icon" src="../../Images/app-window-four-white.png" alt="Blockchain">
+                        <div>Blockchain</div>
+                    </a>    
+                </li>
+            </ul>
+            <div class="hamburger">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+        </div>
+    </div>
+    <div class="pageContainer">
+        <form class="card-form" action="../elaborazioneRichieste.php" method="post">
+            <div class="intestazione">Ritira monete EDU</div>
+            <label for="quantity" class="intestazioneInsert">Inserisci il numero di monete che desideri ritirare dal tuo conto:</label>
+            <div class="card-insert">
+                <input name="quantity" type="number" class="typeText" required>
+                <button name="ritira" value="Ritira" type="submit" class="button-form-send">Ritira</button>
+            </div>
+        </form>
+    </div>
+    <script>
+        const navSlide = () =>{
+            const burger = document.querySelector('.hamburger');
+            const nav = document.querySelector('.nav-links');
+            const navLinks = document.querySelectorAll('.nav-button');
+
+
+            burger.addEventListener('click',()=>{
+                nav.classList.toggle('nav-active');
+                    
+                navLinks.forEach((link,index) =>{
+                    if(link.style.animation){
+                        link.style.animation = "";
+                    }else{
+                        link.style.animation = "navLinkFade 0.3s ease forwards "+(index / 15)+"s";
+                    }
+                });
+                burger.classList.toggle("toggle");
+            });
+                
+        }
+        navSlide();
+    </script>
+</body>
+</html>
